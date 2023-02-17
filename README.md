767f6f271b6e29d3cc7a8fd452c7cd4d54a4e9a9
Phuc Tran <=>
2023-02-17 17:23:59 +0700

    feat: support higher elasticsearch version and opensearch

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
-# pm2-elasticsearch-logger
+# pm2-logger-elasticsearch
 
-[![npm version](https://badge.fury.io/js/pm2-elasticsearch-logger.svg)](https://badge.fury.io/js/pm2-elasticsearch-logger) 
-[![dependencies Status](https://david-dm.org/hobbyquaker/pm2-elasticsearch-logger/status.svg)](https://david-dm.org/hobbyquaker/pm2-elasticsearch-logger)
-[![Build Status](https://travis-ci.org/hobbyquaker/pm2-elasticsearch-logger.svg?branch=master)](https://travis-ci.org/hobbyquaker/pm2-elasticsearch-logger)
+[![npm version](https://badge.fury.io/js/pm2-logger-elasticsearch.svg)](https://badge.fury.io/js/pm2-logger-elasticsearch) 
+[![dependencies Status](https://david-dm.org/tavsta/pm2-logger-elasticsearch/status.svg)](https://david-dm.org/tavsta/pm2-logger-elasticsearch)
+[![Build Status](https://travis-ci.org/tavsta/pm2-logger-elasticsearch.svg?branch=master)](https://travis-ci.org/tavsta/pm2-logger-elasticsearch)
 [![XO code style](https://img.shields.io/badge/code_style-XO-5ed9c7.svg)](https://github.com/sindresorhus/xo)
 [![License][mit-badge]][mit-url]
 
-> Module that sends logs of processes controlled by [PM2](https://pm2.io) to Elasticsearch 📒🔍
+> Module that sends logs of processes controlled by [PM2](https://pm2.io) to Elasticsearch/Opensearch 📒🔍
 
 
 ## Install
 
-`pm2 install pm2-elasticsearch-logger`
+`pm2 install pm2-logger-elasticsearch`
 
 
 ## Configuration
 
 Set options with the command
 
-`pm2 set pm2-elasticsearch-logger:<option> <value>`
+`pm2 set pm2-logger-elasticsearch:<option> <value>`
 
 PM2 will automatically restart the module after changing an option.
 
@@ -26,9 +26,9 @@ PM2 will automatically restart the module after changing an option.
 
 | option |           | default |
 | ------ | --------- | ------- |
-| elasticUrl | URL of Elasticsearch API | `http://localhost:9200` |
+| elasticUrl | URL of Elasticsearch/Opensearch API | `http://localhost:9200` |
 | insecure | allow https connections to servers with invalid certificate | `false` |
-| index | Elasticsearch index to use | `pm2` |
+| index | Elasticsearch/Opensearch index to use | `pm2` |
 | type | Document type to use | `pm2` |
 | host | `host` attribute of the document | `os.hostname()` |
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const os = require('os');
 const pmx = require('pmx');
 const pm2 = require('pm2');
-const request =	require('request');
+const request = require('request');
 
 pmx.initModule({}, (err, conf) => {
     if (err) {
@@ -16,10 +16,33 @@ pmx.initModule({}, (err, conf) => {
         insecure: conf.insecure || false
     };
 
-    let url;
     let currentDate;
+    let isNewElasticsearchVersion = null;
+
+    request.get({
+        url: config.elasticUrl,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        strictSSL: !config.insecure
+    }, (err, res) => {
+        if (err) {
+            console.error('cannot connect to elasticsearch!')
+        } else {
+            const body = JSON.parse(res.body);
+            if (body.version.distribution === 'opensearch' || body.version.number.split('.')[0] >= 6) {
+                isNewElasticsearchVersion = true;
+            } else {
+                isNewElasticsearchVersion = false;
+            }
+        }
+    })
 
     function log(source, msg) {
+        if (isNewElasticsearchVersion === null) {
+            return;
+        }
+
         const d = new Date();
 
         const data = {
@@ -37,22 +60,38 @@ pmx.initModule({}, (err, conf) => {
         if (date !== currentDate) {
             const index = config.index + '-' + d.getFullYear() + '.' + ('0' + (d.getMonth() + 1)).substr(-2) + '.' + ('0' + date).substr(-2);
             console.log('sending logs to', config.elasticUrl + '/' + index + '/' + config.type);
-            url = config.elasticUrl + '/' + index + '/' + config.type + '/';
             currentDate = date;
         }
 
-        request.post({
-            url,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body,
-            strictSSL: !config.insecure
-        }, err => {
-            if (err) {
-                console.error('error on sending log message', err.message);
-            }
-        });
+        if (isNewElasticsearchVersion) {
+            request.put({
+                url: `${config.elasticUrl}/_doc/${Date.now()}/`,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body,
+                strictSSL: !config.insecure
+            }, err => {
+                if (err) {
+                    console.error('error on sending log message', err.message);
+                }
+            });
+
+        } else {
+            request.post({
+                url: `${config.elasticUrl}/${index}/${config.type}/`,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body,
+                strictSSL: !config.insecure
+            }, err => {
+                if (err) {
+                    console.error('error on sending log message', err.message);
+                }
+            });
+
+        }
     }
 
     pm2.launchBus((err, bus) => {
@@ -61,13 +100,13 @@ pmx.initModule({}, (err, conf) => {
         }
 
         bus.on('log:err', data => {
-            if (data.process.name !== 'pm2-elasticsearch-logger') {
+            if (data.process.name !== 'pm2-logger-elasticsearch') {
                 log('stderr', data);
             }
         });
 
         bus.on('log:out', data => {
-            if (data.process.name !== 'pm2-elasticsearch-logger') {
+            if (data.process.name !== 'pm2-logger-elasticsearch') {
                 log('stdout', data);
             }
         });
